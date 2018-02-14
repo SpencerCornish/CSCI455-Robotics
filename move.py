d9acfc38cac4ebc52bd012447f60a00e65e4565e
roboemily <31740786+roboemily@users.noreply.github.com>
2018-02-13 19:14:05 -0700

    add driving speeds

--- move.py
+++ move.py
@@ -10,74 +10,132 @@ servo = Controller()
 root = tk.Tk()
 os.system('xset r off')
 
+#flags for driving
+go_forward = False
+go_backward = False
+
+#driving forward speeds
+one_forward = 5500
+two_forward = 4500
+three_forward = 3500
+
+#driving backward speeds
+one_backward = 6500
+two_backward = 7500
+three_backward = 8500
+
+#neutral speed
+neutral = 6000
+
+#target numbers
+body_target = 0
+drive_target = 1
+turn_target = 2
+neck_target = 3
+head_target = 4
+
+#set targets to neutral
 for i in range(5):
-    servo.setTarget(i,6000)
+    servo.setTarget(i,neutral)
     servo.setSpeed(i,0)
     servo.setAccel(i,60)
 
-servo.setAccel(1, 6)
+servo.setAccel(drive_target, 6)
 
-
+#head looks up
 def w_pressed():
-    servo.setTarget(4,9000)
+    servo.setTarget(head_target,9000)
 def w_released():
-    servo.setTarget(4,6000)
+    servo.setTarget(head_target,neutral)
 
+#head looks down
 def s_pressed():
-    servo.setTarget(4,3000)
+    servo.setTarget(head_target,3000)
 def s_released():
-    servo.setTarget(4,6000)
+    servo.setTarget(head_target,neutral)
 
+#neck turn left
 def a_pressed():
-    servo.setTarget(3,9000)
+    servo.setTarget(neck_target,8900)
 def a_released():
-    servo.setTarget(3,6000)
+    servo.setTarget(neck_target,neutral)
+#neck turn right
 def d_pressed():
-    servo.setTarget(3,3000)
+    servo.setTarget(neck_target,3100)
 def d_released():
-    servo.setTarget(3,6000)
+    servo.setTarget(neck_target,neutral)
 
+#body turn left
 def q_pressed():
-    servo.setTarget(0,9000)
+    servo.setTarget(body_target,8900)
 def q_released():
-    servo.setTarget(0,6000)
+    servo.setTarget(body_target,neutral)
 
+#body turn right
 def e_pressed():
-    servo.setTarget(0,3000)
+    servo.setTarget(body_target,3100)
 def e_released():
-    servo.setTarget(0,6000)
+    servo.setTarget(body_target,neutral)
 
-def up_pressed():
-    servo.setTarget(1,4500)
+#go forwards -- driving
+def up_pressed(speed=4500):
+    go_forward = True
+    servo.setTarget(drive_target,speed)
 def up_released():
-    servo.setTarget(1,6000)
+    go_forward = False
+    servo.setTarget(drive_target,neutral)
 
-def down_pressed():
-    servo.setTarget(1,7500)
+#go backwards -- driving
+def down_pressed(speed=7500):
+    go_backward = True
+    servo.setTarget(drive_target,speed)
 def down_released():
-    servo.setTarget(1,6000)
+    go_backward = False
+    servo.setTarget(drive_target,neutral)
 
+#turn left -- driving
 def left_pressed():
-    servo.setTarget(2,7000)
+    servo.setTarget(turn_target,7000)
 def left_released():
-    servo.setTarget(2,6000)
+    servo.setTarget(turn_target,neutral)
 
-
+#turn right -- driving
 def right_pressed():
-    servo.setTarget(2,5000)
+    servo.setTarget(turn_target,5000)
 def right_released():
-    servo.setTarget(2,6000)
+    servo.setTarget(turn_target,neutral)
 
+#set all servos/motors to neutral
+def space_pressed():
+    for i in range(5):
+        servo.setTarget(i,neutral)
 
+#set driving speed to speed 1
+def one_pressed():
+    if go_forward:
+        up_pressed(one_forward)
+    elif go_backward:
+        down_pressed(one_backward)
 
+#set driving speed to speed 2
+def two_pressed():
+    if go_forward:
+        up_pressed(two_forward)
+    elif go_backward:
+        down_pressed(two_backward)
 
+#set driving speed to speed 3
+def three_pressed():
+    if go_forward:
+        up_pressed(three_forward)
+    elif go_backward:
+        down_pressed(three_backward)
 
-
-
+#get value of key pressed, execute correct function
 def key_pressed(e):
     key = e.keysym
     print(key)
-    if(key == 'escape'):
+    if(key == 'Escape'):
         os.system('xset r on')
         sys.exit(0)
     elif(key == 'Up'):
@@ -100,41 +158,42 @@ def key_pressed(e):
         q_pressed()
     elif(key == 'e'):
         e_pressed()
+    elif(key == 'space'):
+        space_pressed()
+    elif(key == '1'):
+        one_pressed()
+    elif(key == '2'):
+        two_pressed()
+    elif(key == '3'):
+        three_pressed()
 
-
-
-
+#get value of key released, execute correct function
 def key_released(e):
-        key = e.keysym
-        if(key == 'Up'):
-            up_released()
-        elif(key == 'Down'):
-            down_released()
-        elif(key == 'Left'):
-            left_released()
-        elif(key == 'Right'):
-            right_released()
-        elif(key == 'w'):
-            w_released()
-        elif(key == 's'):
-            s_released()
-        elif(key == 'a'):
-            a_released()
-        elif(key == 'd'):
-            d_released()
-        elif(key == 'q'):
-            q_released()
-        elif(key == 'e'):
-            e_released()
-
-
-
+    key = e.keysym
+    if(key == 'Up'):
+        up_released()
+    elif(key == 'Down'):
+        down_released()
+    elif(key == 'Left'):
+        left_released()
+    elif(key == 'Right'):
+        right_released()
+    elif(key == 'w'):
+        w_released()
+    elif(key == 's'):
+        s_released()
+    elif(key == 'a'):
+        a_released()
+    elif(key == 'd'):
+        d_released()
+    elif(key == 'q'):
+        q_released()
+    elif(key == 'e'):
+        e_released()
 
 root.bind('<KeyPress>', key_pressed)
 root.bind('<KeyRelease>', key_released)
 root.mainloop()
-
-
 
 
 
