cc7c2003817d3b643bd0dd0a28cebae0e6e30180
roboemily <31740786+roboemily@users.noreply.github.com>
2018-04-19 15:04:55 -0600

    finish game logic with command line

--- android/app/src/main/java/com/example/spencercornish/roboticsapp/Node.java
+++ android/app/src/main/java/com/example/spencercornish/roboticsapp/Node.java
@@ -4,11 +4,11 @@ public class Node {
     public Controller.activity Activity;
     public String name;
     public String[] directions;
+    public boolean visited = false;
 
     public Node(Controller.activity Activity, String name, String[] directions){
         this.Activity = Activity;
         this.name = name;
         this.directions = directions;
     }
-
 }
